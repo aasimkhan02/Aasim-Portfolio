0fbdda56bd289df4a0d4ea19f5aca2c51fa84d16
aasimkhan02 <aasimkroc@gmail.com>
2025-06-17 21:28:18 +0530

    updated ui

--- src/Pages/About/AboutPage.jsx
+++ src/Pages/About/AboutPage.jsx
@@ -40,6 +40,7 @@ const About = () => {
             </div>
             <div className="Timeline">
                 <h1>My Timeline</h1>
+                <div class="vertical-line"></div>
             </div>
         </div>
     )
