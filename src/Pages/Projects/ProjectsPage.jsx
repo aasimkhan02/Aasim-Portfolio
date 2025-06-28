c6776f6f6e195c06ff34121e50e2ab4cf3fab0cb
aasimkhan02 <aasimkroc@gmail.com>
2025-06-28 16:44:52 +0530

    new update

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './ProjectsPage.css'
+import HealthWave1 from './../../assets/healthwave1.jpg'
 
 const ProjectsPage = () => {
+  const [activeCategory, setActiveCategory] = useState('All')
+  const categories = ['All', 'Web', 'Machine Learning']
   return (
     <div className='ProjectPage'>
         <h1>Projects that speak for my skills</h1>
+        <div className="Project-categories">
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={activeCategory === category ? 'active' : ''}
+            onClick={() => setActiveCategory(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+      <div className="All-Projects">
+        <div className="HealthWave">
+          <img src={HealthWave1} alt="" />
+        </div>
+      </div>
     </div>
   )
 }
