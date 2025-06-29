58a643201a1520c8cd8e25f243b4094b7aee56f2
aasimkhan02 <aasimkroc@gmail.com>
2025-06-29 16:11:13 +0530

    new updates

--- src/Pages/Projects/ProjectsPage.jsx
+++ src/Pages/Projects/ProjectsPage.jsx
@@ -7,7 +7,7 @@ const ProjectsPage = () => {
   const categories = ['All', 'Web', 'Machine Learning']
   return (
     <div className='ProjectPage'>
-        <h1>Projects that speak for my skills</h1>
+        <h1 className="ProjectPageHeadline">Projects that speak for my skills</h1>
         <div className="Project-categories">
         {categories.map((category) => (
           <button
@@ -22,6 +22,14 @@ const ProjectsPage = () => {
       <div className="All-Projects">
         <div className="HealthWave">
           <img src={HealthWave1} alt="" />
+          <div className="project-details-header">
+            <h1 className='Project-year'>2024</h1>
+            <h1 className="Project-name">
+              <span className='PrjTitle'>HealthWave</span> / Healthcare <br />
+              <span className="Project-tags">Web, Machine learning</span>
+            </h1>
+            <h1 className='Project-number'>01</h1>
+          </div>
         </div>
       </div>
     </div>
